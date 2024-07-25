aebe33b598dcf408011761a41c3008da1c03aba7
Albert Byiringiro <byiringiroalbert20@gmail.com>
2024-07-25 18:07:29 +0200

    update state to take array of object

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,49 @@
 import Die from "./components/Die";
-import { useState, useEffect, useId } from "react";
+import { useState} from "react";
+import {nanoid} from "nanoid"
 
 export default function App() {
 
-  
-  /**
-   * Challenge:
-   * 
-   * Create state to hold our array of numbers. (Initialize
-   * the state by calling our `allNewDice` function so it 
- * loads all new dice as soon as the app loads)
+/**
+ * Challenge: Update the array of numbers in state to be
+ * an array of objects instead. Each object should look like:
+ * { value: <random number>, isHeld: false }
  * 
- * Map over the state numbers array to generate our array
- * of Die elements and render those in place of our
- * manually-written 10 Die elements.
+ * Making this change will break parts of our code, so make
+ * sure to update things so we're back to a working state
  */
 
-  const allNewDice = () => {
+  const allNewDice1 = () => {
     const diceArray = [];
     for (let i = 0; i < 10; i++) {
       const calculateRandom = Math.ceil(Math.random() * 6)
-      diceArray.push(calculateRandom)
+      let diceObject = {id: nanoid(), value: calculateRandom, isHeld: false}
+      diceArray.push(diceObject)
     }
     return diceArray
   } 
+ 
+  
+  const [dice1, setdice1] = useState(allNewDice1)
 
-  
-  const [dice, setdice] = useState(allNewDice)
-  
-  const diceDisplay = dice.map((die, index) => {
-    return <Die key={index} value={die}/>
+
+  const handlerRoll = () => {
+    setdice1(allNewDice1)
+  }
+
+  const diceDisplay1 = dice1.map(die => {
+    return <Die key={die.id} value={die.value}/>
   })
 
-  console.log(diceDisplay)
+
+  
   
   return (
     <main className='bg-white h-[500px] w-[800px] flex flex-col justify-center items-center'>
       <div className="grid grid-cols-5 grid-rows-2 gap-5">
-        {diceDisplay}
+        {diceDisplay1}
       </div>
+      <button className="bg-[#5035FF] text-white px-10 py-2 mt-10 text-3xl font-semibold text-center cursor-pointer rounded-md" onClick={handlerRoll}>Roll</button>
     </main>    
 
   )
