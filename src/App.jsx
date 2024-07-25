83362bff145dff0bfbcd91619facb10ddb694c17
Albert Byiringiro <byiringiroalbert20@gmail.com>
2024-07-25 22:49:33 +0200

    add set tenzies to true

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,20 @@
 import Die from "./components/Die";
-import { useState} from "react";
+import { useEffect, useState} from "react";
 import {nanoid} from "nanoid"
 
 export default function App() {
 
-/**
- * Challenge: Add conditional styling to the Die component
- * so that if it's held (isHeld === true), its background color
- * changes to a light green (#59E391)
- * 
- * Remember: currently the Die component has no way of knowing
- * if it's "held" or not.
- */
+  const generateDie = () => {
+    return {
+      id: nanoid(), 
+      value: Math.ceil(Math.random() * 6), 
+      isHeld: false}
+  }
 
   const allNewDice1 = () => {
     const diceArray = [];
     for (let i = 0; i < 10; i++) {
-      const calculateRandom = Math.ceil(Math.random() * 6)
-      let diceObject = {id: nanoid(), value: calculateRandom, isHeld: true}
-      diceArray.push(diceObject)
+      diceArray.push(generateDie())
     }
     return diceArray
   } 
@@ -27,21 +23,67 @@ export default function App() {
   const [dice1, setdice1] = useState(allNewDice1)
 
 
-  const handlerRoll = () => {
-    setdice1(allNewDice1)
+  /**
+ * Challenge:
+ * 1. Add new state called `tenzies`, default to false. It
+ *    represents whether the user has won the game yet or not.
+ * 2. Add an effect that runs every time the `dice` state array 
+ *    changes. For now, just console.log("Dice state changed").
+ */
+const [tenzies, setTenzies] = useState(false);
+
+
+/**
+ * Challenge: Check the dice array for these winning conditions:
+ * 1. All dice are held, and
+ * 2. all dice have the same value
+ * 
+ * If both conditions are true, set `tenzies` to true and log
+ * "You won!" to the console
+ */
+useEffect(()=>{
+  const isHandled = dice1.every(die => die.isHeld);
+  const firstValue = dice1[0].value;
+  const sameValue = dice1.every(die => firstValue === die.value);
+
+  if (isHandled && sameValue) {
+    setTenzies(true);
+    console.log("Won!");
   }
+}, [dice1])
+  
 
-  const diceDisplay1 = dice1.map(die => {
-    console.log(die.isHeld)
-    return <Die key={die.id} value={die.value} isHeld={die.isHeld}/>
-  })
 
+
+  const handlerRoll = () => {
+    setdice1(oldDice => oldDice.map(die => {
+      return die.isHeld ? die : generateDie();
+    }))
+  }
+  
 
   
+
+  const holdDice = (id) => {
+    setdice1(prevDice => {
+      return prevDice.map(die => die.id === id ? {...die, isHeld: !die.isHeld} : die)
+    })
+  }
+  
+  const diceDisplay1 = dice1.map(die => {
+    return <Die 
+      key={die.id} 
+      value={die.value} 
+      isHeld={die.isHeld} 
+      holdDice={() => holdDice(die.id)} />
+  })
   
   return (
+    
     <main className='bg-white h-[500px] w-[800px] flex flex-col justify-center items-center'>
-      <div className="grid grid-cols-5 grid-rows-2 gap-5">
+      <h1 className="m-0 text-4xl font-semibold text-gray-900">Tenzies</h1>
+      <p className="mt-0 font-normal text-center">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+      <div className="grid grid-cols-5 grid-rows-2 gap-5 mt-8">
         {diceDisplay1}
       </div>
       <button className="bg-[#5035FF] text-white px-10 py-2 mt-10 text-3xl font-semibold text-center cursor-pointer rounded-md" onClick={handlerRoll}>Roll</button>
--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -4,7 +4,7 @@ export default function Die(props) {
         backgroundColor: props.isHeld ? "#59E391" : "white",
     }
     return(
-        <div className="flex items-center justify-center w-12 h-12 text-3xl font-bold shadow-md cursor-pointer rounded-xl" style={styles}>
+        <div onClick={props.holdDice} className="flex items-center justify-center w-12 h-12 text-3xl font-bold shadow-md cursor-pointer rounded-xl" style={styles}>
         <h2>{props.value}</h2>
         </div>
     );
